4bd7615dd50e03450c84ec4f2174b2797afc9b8f
Wil <winterzuber11@gmail.com>
2024-09-08 17:43:31 -0700

    Remove bad up arrow

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,8 @@
-// scroll to top functionality
-const scrollUp = document.querySelector("#scroll-up");
 
-scrollUp.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth",
-    })
-});
 
 // Nav hamburger selection
 const burger = document.querySelector("#burger-menu");
 const ul = document.querySelector("nav ul");
-const nav = document.querySelector("nav");
 
 burger.addEventListener("click", () => {
     ul.classList.toggle("show");
@@ -20,11 +10,4 @@ burger.addEventListener("click", () => {
 
 // Close hamburger menu when a link is clicked
 
-// Select nav links
-const navLink = document.querySelectorAll(".nav-link");
 
-navLink.forEach((link) =>
-    link.addEventListener("click", () => {
-        ul.classList.remove("show");
-    })
-);
